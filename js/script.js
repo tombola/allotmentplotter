bf46dc7f90b3e295c0ed95b0cb0542a3c3c9d464
tombola <thomas.readings@gmail.com>
2018-06-16 11:44:15 +0100

    Draw a row guide

---
+++ js/script.js
@@ -0,0 +1,56 @@
+$allotment = d3.select("#allotment");
+  plantingRow = [ 2, 6, 8, 4, 6, 8, 3, 4]
+  plotWidth = 500;
+  plotHeight = 400;
+  plantingDistance = 30;
+  rowDistance = 40;
+
+  var $plot = $allotment.append('svg')
+    .attr('width', plotWidth)
+    .attr('height', plotHeight)
+
+  $plants = $plot.selectAll('circle')
+    .data(plantingRow)
+    .enter()
+    .append('circle')
+    .attr("fill", 'none')
+    .attr("stroke", '#000')
+
+  $plants.attr('cy', function(d, i) {
+        return ((i + 1) * plantingDistance);
+    })
+   .attr('cx', rowDistance)
+   .attr("r", function(d) {
+        return d;
+   });
+
+  var rowGuideLine = function(align, position) {
+    var lineFunction = d3.line()
+      .x(function(d) { return d.x; })
+      .y(function(d) { return d.y; })
+      .curve(d3.curveLinear);
+
+    if (align == 'vertical') {
+      var guideData = [
+        { x: position, y: 0},
+        { x: position, y: 1000},
+      ];
+    }
+    else if (align == 'horizontal') {
+      var guideData = [
+        { x: 0, y: position},
+        { x: 1000, y: position},
+      ];
+    }
+    return lineFunction(guideData)
+  }
+
+  // TODO: Create guides based on array
+  // TODO: Use plot data for array
+  var drawGuides = function($plot, guideArray) {
+    var $guides = $plot.append("path")
+      .attr("d", rowGuideLine('vertical', 200))
+      .attr("class", "row-guide")
+  }
+
+  drawGuides($plot, [])
