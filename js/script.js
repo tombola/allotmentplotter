a3a84045d686dae2ca0c0fc1b18a1d70e5e01daf
tombola <thomas.readings@gmail.com>
2018-06-16 17:26:13 +0100

    Add multiple guides from array

--- js/script.js
+++ js/script.js
@@ -1,57 +1,65 @@
-$allotment = d3.select("#allotment");
-  plantingRow = [ 2, 6, 8, 4, 6, 8, 3, 4]
-  plotWidth = 500;
-  plotHeight = 400;
-  plantingDistance = 30;
-  rowDistance = 40;
+var $allotment = d3.select("#allotment");
 
-  var $plot = $allotment.append('svg')
-    .attr('width', plotWidth)
-    .attr('height', plotHeight)
+var plantingRow = [ 2, 6, 8, 4, 6, 8, 3, 4]
+var plotWidth = 500;
+var plotHeight = 400;
+var plantingDistance = 30;
+var rowDistance = 40;
+var guides = [
+  {align:'vertical', position:100},
+  {align:'vertical', position:240},
+  {align:'vertical', position:40},
+  {align:'horizontal', position:100},
+  {align:'horizontal', position:300},
+]
 
-  $plants = $plot.selectAll('circle')
-    .data(plantingRow)
-    .enter()
-    .append('circle')
-    .attr("fill", 'none')
-    .attr("stroke", '#000')
-    .attr("class", "plant")
+var $bed = $allotment.append('svg')
+  .attr('width', plotWidth)
+  .attr('height', plotHeight)
+  .attr("class", "bed")
 
-  $plants.attr('cy', function(d, i) {
-        return ((i + 1) * plantingDistance);
-    })
-   .attr('cx', rowDistance)
-   .attr("r", function(d) {
-        return d;
-   });
+$plants = $bed.selectAll('circle')
+  .data(plantingRow)
+  .enter()
+  .append('circle')
+  .attr("fill", 'none')
+  .attr("stroke", '#000')
+  .attr("class", "plant")
 
-  var rowGuideLine = function(align, position) {
-    var lineFunction = d3.line()
-      .x(function(d) { return d.x; })
-      .y(function(d) { return d.y; })
-      .curve(d3.curveLinear);
+$plants.attr('cy', function(d, i) {
+      return ((i + 1) * plantingDistance);
+  })
+ .attr('cx', rowDistance)
+ .attr("r", function(d) {
+      return d;
+ });
 
-    if (align == 'vertical') {
-      var guideData = [
-        { x: position, y: 0},
-        { x: position, y: 1000},
-      ];
-    }
-    else if (align == 'horizontal') {
-      var guideData = [
-        { x: 0, y: position},
-        { x: 1000, y: position},
-      ];
-    }
-    return lineFunction(guideData)
+var rowGuideLine = function(align, position) {
+  var lineFunction = d3.line()
+    .x(function(d) { return d.x; })
+    .y(function(d) { return d.y; })
+    .curve(d3.curveLinear);
+
+  if (align == 'vertical') {
+    var guideData = [
+      { x: position, y: 0},
+      { x: position, y: 1000},
+    ];
   }
-
-  // TODO: Create guides based on array
-  // TODO: Use plot data for array
-  var drawGuides = function($plot, guideArray) {
-    var $guides = $plot.append("path")
-      .attr("d", rowGuideLine('vertical', 200))
-      .attr("class", "row-guide")
+  else if (align == 'horizontal') {
+    var guideData = [
+      { x: 0, y: position},
+      { x: 1000, y: position},
+    ];
   }
+  return lineFunction(guideData)
+}
 
-  drawGuides($plot, [])
+// Take the array of guides and draw paths on  
+var plotGuides = $bed.selectAll('.row-guide')
+  .data(guides)
+  .enter()
+  .append("path")
+  .attr("d", function(d,i) { return rowGuideLine(d.align, d.position)})
+  .attr("class", "row-guide")
+
