18137c63ebd7c5eadb8cd2ebe29bd3dea2a26cb3
tombola <thomas.readings@gmail.com>
2022-06-10 13:36:31 +0100

    Add plant model with optional decimal month ranges

--- plotter/plants/models.py
+++ plotter/plants/models.py
@@ -6,15 +6,42 @@ from django.db import models
 
 
 class Plant(models.Model):
-    """Plant
+    """
     Represents a plant type, eg courgette or asparagus.
 
-    Details such as planting times can be overwritten by more specific `PlantVariety`.
+    Details such as planting times can be overwridden by more specific `PlantVariety`.
     """
 
     common_name = models.CharField(max_length=255)
+    latin_name = models.CharField(max_length=255)
 
-    sow_indoors = models.DecimalField()
-    transplant = models.DecimalField()
-    sow_outdoors = models.DecimalField()
-    harvest = models.DecimalField()
+    # Month planting guidelines use 1-12
+    # numeric options allows ranges
+    # decimal places allow early/late month descriptions
+    sow_indoors_earliest = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2
+    )
+    sow_indoors_latest = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2
+    )
+    transplant_earliest = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2
+    )
+    transplant_latest = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2
+    )
+    sow_outdoors_earliest = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2
+    )
+    sow_outdoors_latest = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2
+    )
+    harvest_earliest = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2
+    )
+    harvest_latest = models.DecimalField(
+        null=True, blank=True, max_digits=4, decimal_places=2
+    )
+
+    def __str__(self):
+        return self.common_name
