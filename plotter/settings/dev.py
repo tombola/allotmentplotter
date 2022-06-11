b3ed3891ea11df6741962cb65a0b34d139c4a9e3
tombola <thomas.readings@gmail.com>
2022-06-11 17:19:22 +0100

    Add dev dependencies

--- plotter/settings/dev.py
+++ plotter/settings/dev.py
@@ -1,9 +1,11 @@
 from .base import *
 
 DEBUG = True
-SECRET_KEY = 'something'
-ALLOWED_HOSTS = ['*']
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SECRET_KEY = "something"
+ALLOWED_HOSTS = ["*"]
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+INSTALLED_APPS.append("django_extensions")
 
 try:
     from .local import *
