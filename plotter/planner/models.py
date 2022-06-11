21e542109246f66415721f357e0acf4ab8419ada
tombola <thomas.readings@gmail.com>
2022-06-11 17:18:53 +0100

    Add bed plantings

--- plotter/planner/models.py
+++ plotter/planner/models.py
@@ -1,3 +1,14 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+
+class Bed(models.Model):
+    width = models.DecimalField(max_digits=5, decimal_places=2)
+    length = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class BedPlanting(models.Model):
+    bed = models.ForeignKey("planner.Bed", on_delete=models.CASCADE)
+    plant = models.ForeignKey("plants.Plant", on_delete=models.SET_NULL, null=True)
+    planting_date = models.DateField()
+    harvest_date = models.DateField()
